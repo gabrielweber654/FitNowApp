eeee082754dc5bd768261b758c6b05814f36f890
GabrielWeber <gabrielweber654@gmail.com>
2021-11-29 12:45:46 -0800

    11-29 commit visual design of every screen except main menu screen has been implemented and organized Huge amounts of organization within code, lots of extraneous information removed

--- app/src/main/java/com/example/navigation_slides/ui/bodybuilding/BodybuildingFragment.java
+++ app/src/main/java/com/example/navigation_slides/ui/bodybuilding/BodybuildingFragment.java
@@ -4,35 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-
-import com.example.navigation_slides.R;
 import com.example.navigation_slides.databinding.FragmentBodybuildingBinding;
 
 public class BodybuildingFragment extends Fragment {
 
-    private BodybuildingViewModel bodybuildingViewModel;
     private FragmentBodybuildingBinding binding;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        bodybuildingViewModel =
-                new ViewModelProvider(this).get(BodybuildingViewModel.class);
-
         binding = FragmentBodybuildingBinding.inflate(inflater, container, false);
-
-
-
         View root = binding.getRoot();
-
-        final TextView textView = binding.textBodybuilding;
-        bodybuildingViewModel.getText().observe(getViewLifecycleOwner(), s -> textView.setText(s));
         return root;
     }
 
@@ -42,16 +24,4 @@ public class BodybuildingFragment extends Fragment {
         binding = null;
     }
 }
-
-
-        /*    <com.google.android.material.floatingactionbutton.FloatingActionButton
-android:id="@+id/fab"
-android:layout_width="wrap_content"
-android:layout_height="wrap_content"
-android:layout_gravity="bottom|end"
-android:layout_marginEnd="@dimen/fab_margin"
-android:layout_marginRight="@dimen/fab_margin"
-android:layout_marginBottom="16dp"
-app:srcCompat="@drawable/add"
-tools:ignore="SpeakableTextPresentCheck" />*/
 
--- app/src/main/java/com/example/navigation_slides/ui/cardio/CardioFragment.java
+++ app/src/main/java/com/example/navigation_slides/ui/cardio/CardioFragment.java
@@ -4,33 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-
-import com.example.navigation_slides.R;
 import com.example.navigation_slides.databinding.FragmentCardioBinding;
 
 public class CardioFragment extends Fragment {
 
-    private CardioViewModel cardioViewModel;
     private FragmentCardioBinding binding;
-
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        cardioViewModel =
-                new ViewModelProvider(this).get(CardioViewModel.class);
-
         binding = FragmentCardioBinding.inflate(inflater, container, false);
-
         View root = binding.getRoot();
-        final TextView textView = binding.textCardio;
-        cardioViewModel.getText().observe(getViewLifecycleOwner(), s -> textView.setText(s));
         return root;
     }
 
